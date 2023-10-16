7924ff9ae8aaec666d031d0387d7e3b47118f842
PrebenS <prebens.kodehode@gmail.com>
2023-10-16 09:34:39 +0200

    changed portfolio file name

--- routes/root.js
+++ routes/root.js
@@ -15,4 +15,8 @@ router.get("/new-page(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "view", "new-page.html"));
 });
 
+router.get("/portfolio(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "view", "portfolio.html"));
+});
+
 module.exports = router;
